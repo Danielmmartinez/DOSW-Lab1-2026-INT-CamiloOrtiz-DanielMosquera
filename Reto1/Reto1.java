306b2c40e248c8602f70dc31211fa4a6169e2848
unknown <cristian.ortiz-s@labinfo.is.escuelaing.edu.co>
2026-08-04 13:26:00 -0500

    Reto 1 sin scanner

---
+++ Reto1/Reto1.java
@@ -0,0 +1,69 @@
+package main;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Reto1 {
+
+	public static void main(String[] args) {
+		
+		List<Estudiante> estudiantes = List.of(
+				new Estudiante("Camilo Ortiz", 18, "cristian.ortiz-s@mail.escuelaing.edu.co", 6),
+				new Estudiante("Daniel Mosquera", 19, "daniel.mmartinez@mail.escuelaing.edu.co", 6));
+		
+		MensajeBienvenida.imprimirMensajeBienvenida(estudiantes);
+	}
+}
+
+class Estudiante {
+	
+	private String nombre;
+	private int edad;
+	private String correo;
+	private int semestre;
+	
+	public Estudiante(String nombre, int edad, String correo, int semestre) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.correo = correo;
+		this.semestre = semestre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public int getSemestre() {
+		return semestre;
+	}
+	
+}
+
+class MensajeBienvenida {
+	
+	public static void imprimirMensajeBienvenida(List<Estudiante> estudiantes) {
+		
+		System.out.println("¡Hola, bienvenidos! Somos la pareja conformada por");
+		
+		System.out.print(
+			estudiantes.stream()
+				   	   .map(estudiante -> estudiante.getNombre() + ", estudiante de " + estudiante.getSemestre()
+						   + "° semestre de " + estudiante.getEdad() + " años")
+				       .collect(Collectors.joining(", y ")));
+		
+		System.out.println(". Nuestros correos son:");
+		
+		System.out.print(
+				estudiantes.stream()
+					   	   .map(estudiante -> estudiante.getCorreo())
+					       .collect(Collectors.joining(" y ")));
+	}
+}

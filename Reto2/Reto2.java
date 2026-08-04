8a1f555a8d90ab26addabe7a451347f275053966
Cristian Ortiz <cristian.ortiz-s@mail.escuelaing.edu.co>
2026-08-04 14:55:31 -0500

    Reto 2 sin scanner

---
+++ Reto2/Reto2.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.List;
+
+public class Reto2 {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> lista1 = List.of(14, 3, 27, 8, 51, 6);
+		
+		int maximo = lista1.stream()
+						   .max((a, b) -> Integer.compare(a, b))
+						   .get();
+		
+		System.out.println("Lista 1 => max:" + maximo);
+	}
+}

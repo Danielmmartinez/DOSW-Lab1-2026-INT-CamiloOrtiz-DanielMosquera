d6c799cd1de11c65b4fd09d27dbf26f4bc89baef
Daniel Mosquera <daniel.mmartinez@mail.escuelaing.edu.co>
2026-08-07 14:26:12 -0500

    Reto3 culminado

--- Reto3/Reto3.java
+++ Reto3/Reto3.java
@@ -1,12 +1,33 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Reto3 {
 	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Mensaje interceptado: ");
+		String mensaje = amplificador(scanner);
+		descifrar(mensaje);
+
 		
 	}
+	public static String amplificador(Scanner scanner){
+		String mensaje = scanner.nextLine();
+		StringBuilder mensajeAmplificado = new StringBuilder();
+		Collections.nCopies(3, mensaje)
+				.stream()
+				.forEach(palabra ->
+						mensajeAmplificado.append(palabra).append(" "));
+		String amplificado = mensajeAmplificado.toString().trim();
+		System.out.println("Canal A: " + amplificado);
+		return amplificado;
+	}
 	
-	private String descifrar(String mensaje) {
+	private static void descifrar(String mensaje) {
 		
 		StringBuffer mensajeDescifrado = new StringBuffer(mensaje);
-		return mensajeDescifrado.reverse().toString();
+		System.out.println("\nDescifrado final:\n" +
+				mensajeDescifrado.reverse().toString());
 	}
 }

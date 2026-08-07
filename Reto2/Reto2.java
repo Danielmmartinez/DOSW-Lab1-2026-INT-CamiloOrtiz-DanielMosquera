9fa7d261ce31fac618c26ff0561bd4763ddf8608
Cristian Camilo Ortiz Sánchez <cristian.ortiz-s@mail.escuelaing.edu.co>
2026-08-06 22:04:10 -0500

    Implementacion método múltiploDeDos para carril1

--- Reto2/Reto2.java
+++ Reto2/Reto2.java
@@ -10,23 +10,27 @@ public class Reto2 {
 		mostrarDatos(lista1, "Lista 1");
 		mostrarDatos(lista2, "Lista 2");
 	}
-	public static void mostrarDatos(List<Integer> lista, String nombre){
+
+	public static void mostrarDatos(List<Integer> lista, String nombre) {
 
 		int minimo = lista.stream()
-				.min((a,b) -> Integer.compare(a,b))
-				.get();
+						  .min((a,b) -> Integer.compare(a,b))
+						  .get();
 
-		long cantidad = lista.stream()
-				.count();
+		long cantidad = lista.stream().count();
 		
 		int maximo = lista.stream()
-						   .max((a, b) -> Integer.compare(a, b))
-						   .get();
+						  .max((a, b) -> Integer.compare(a, b))
+						  .get();
 
 		System.out.println(
-				nombre + "=> max:" + maximo +
+				nombre + " => max:" + maximo +
 						" min:" + minimo +
 						" cant:" + cantidad
 		);
+
+		System.out.println("  ¿" + maximo + " múltiplo de 2? " + multiploDeDos(maximo));
 	}
+
+	private static String multiploDeDos(Integer numero) { return numero % 2 == 0 ? "Sí" : "No"; }
 }
